a8124731ae884cda05fd523a7b63ee61f8f26a4e
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-02-02 15:38:13 -0800

    adding second compation aka sub-object

--- index.js
+++ index.js
@@ -5,17 +5,21 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
-    }
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    },
+}
 
 console.log(adventurer.inventory[0]);
 
-// Create a loop that logs each item in Robin’s inventory.
+
+// 1.1 Create a loop that logs each item in Robin’s inventory.
 
 for (let i=0; i < adventurer.inventory.length; i++) {
     console.log(adventurer.inventory[i])
 }
-
 
 
 // also works:
@@ -24,3 +28,24 @@ for (let i=0; i < adventurer.inventory.length; i++) {
 //         RobinsInventory[i] = `Robin's inventory consists of: ${adventurer.inventory}`;
 //     }
 //     console.log(RobinsInventory[0])         // works
+
+
+// 1.2 Adding a friend Leo (added at the top (updated the initial data provided in the lab)) + checking
+
+console.log(adventurer.companion)
+
+
+// 1.3 Next, give Robin’s feline friend a friend of his own:
+        // Add a “companion” sub-object to “Leo” with the following properties:
+        // The companion’s name is “Frank.”
+        // The companion’s type is “Flea.”
+        // The companion has its own belongings, which includes a small hat and sunglasses.
+
+adventurer.companion.companion = {
+    name: "Frank",
+    type: "Flea",
+    inventory: ["hat", "sunglasses"]
+};
+
+console.log(adventurer)
+console.log(adventurer.companion)
